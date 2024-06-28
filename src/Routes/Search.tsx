647df2b293a18e7e12857bca918b65a89efed12d
yoonseo choi <seby731@naver.com>
2024-06-28 17:29:24 +0900

    second commit

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,13 +1,13 @@
 import { useLocation } from "react-router-dom";
 
-function Search(){
-    const location = useLocation();
-    const keyword = new URLSearchParams(location.search).get("keyword");
+function Search() {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
 
-    //multi search
-    
-    console.log(keyword);
-    return null;
+  //multi search
+
+  console.log(keyword);
+  return <div>추가했다이부분</div>;
 }
 
 export default Search;
